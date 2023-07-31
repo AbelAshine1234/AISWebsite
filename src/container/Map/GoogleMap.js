6d7cf5ce864fa591a86a72c80d915ea729b7b49e
abelashine <abelashine@lablaco.com>
2023-07-31 16:55:46 +0300

    hi

---
+++ src/container/Map/GoogleMap.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const GoogleMap = () => {
+    return (
+        <div className="google-map-area section text-center section-padding-bottom">
+            <div className="container">
+                <div className="contact-map-area" data-aos="fade-up">
+                    <iframe className="contact-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.322358814194!2d38.75681681475525!3d9.02456629295373!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x163fcf3d15e8da31%3A0xde0b56f156ae9d35!2sGolagul%20Building%20-%20%E1%8C%8E%E1%88%8B%E1%8C%89%E1%88%8D%E1%88%8D%20%E1%88%85%E1%8A%95%E1%8D%83!5e0!3m2!1sen!2set!4v1627494352361!5m2!1sen!2set"  aria-hidden="false"></iframe>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default GoogleMap;
